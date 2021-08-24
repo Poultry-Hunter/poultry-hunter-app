f881ab63b180f2bd1442c330904ffd29e7d6ca5a
SairajK19 <saikapdi11@gmail.com>
2021-08-24 16:28:06 +0530

    Added font

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,4 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-export const App = (): JSX.Element => <div>Hello Ts</div>;
+import "./App.css";
+
+export const App = (): JSX.Element => (
+  <>
+    <div>
+      <h1>Hello TS</h1>
+    </div>
+  </>
+);
