6429e4c684461243a20a042875d42c1085fd61ea
c0deb1ind31 <rhutikparab1@gmail.com>
2021-08-24 18:01:48 +0530

    minor change

--- program/src/state.rs
+++ program/src/state.rs
@@ -12,13 +12,26 @@ pub struct Distributor {
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Seller {
-    name: String,
+    shop_name: String,
+    owner_name: String,
+    shop_address: String,
+    owner_contact: String,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct HealthProffessional {
+pub struct HealthOfficer {
     name: String,
+    office_id: String,
+    office_address: String,
+    officer_contact: String,
 }
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
 
-pub struct Batch {}
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Batch {
+    batch_id: u32,
+    farm_pubkey: String,
+    distributor_pubkey: String,
+    seller_pubkey: String,
+    generated_at: String,
+    sold_at: String,
+}
