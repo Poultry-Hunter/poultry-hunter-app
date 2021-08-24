90fb7302d76402d1d3d26ab76645f544ba22bdda
SairajK19 <saikapdi11@gmail.com>
2021-08-24 18:03:02 +0530

    added farm parameters

--- program/src/state.rs
+++ program/src/state.rs
@@ -2,19 +2,22 @@ use borsh::{BorshDeserialize, BorshSerialize};
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Farm {
-    name: String,
+	farm_name: String,
+	owner_name: String,
+	contact_number: u8,
+	
 }
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Distributor {
-    name: String,
+	name: String,
 }
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Seller {
-    name: String,
+	name: String,
 }
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct HealthProffessional {
-    name: String,
+	name: String,
 }
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct Batch {}
