e3f8109532bdd53dffaf912d09eb9238763977f8
SairajK19 <saikapdi11@gmail.com>
2021-08-28 18:39:12 +0530

    Added wallet adapter 🚀️

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,50 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React, { useEffect, useState, useMemo } from "react";
+import ConnectWallet from "../../ConnectWallet";
+
+import {
+  clusterApiUrl,
+} from "@solana/web3.js";
+
+import {
+  WalletDialogProvider,
+} from "@solana/wallet-adapter-material-ui";
+
+import {
+  WalletProvider,
+  ConnectionProvider,
+} from "@solana/wallet-adapter-react";
+
+import {
+  getPhantomWallet,
+  getSolflareWebWallet,
+  getTorusWallet,
+  getSolletWallet,
+} from "@solana/wallet-adapter-wallets";
 
 import "./App.css";
 
-export const App = (): JSX.Element => (
-  <>
-    <div>
-      <h1>Hello TS</h1>
-    </div>
-  </>
-);
+export const App = (): JSX.Element => {
+  const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+
+  const wallets = useMemo(
+    () => [
+      getPhantomWallet(),
+      getSolflareWebWallet(),
+      // getTorusWallet(),
+      getSolletWallet(),
+    ],
+    []
+  );
+
+  return (
+    <>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletDialogProvider>
+            <ConnectWallet />
+          </WalletDialogProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </>
+  );
+};
