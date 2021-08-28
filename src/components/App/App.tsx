79a6cebf8972f0d7a0fe7780d3646b5a264812cc
c0deb1ind31 <rhutikparab1@gmail.com>
2021-08-28 21:54:56 +0530

    test borsh

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
+import { DistributorAccount, SCHEMA } from "../../schema";
+import * as borsh from "borsh";
 import "./App.css";
 
-export const App = (): JSX.Element => (
-  <>
-    <div>
-      <h1>Hello TS</h1>
-    </div>
-  </>
-);
+export const App = (): JSX.Element => {
+  const data = new DistributorAccount(
+    "rhutik distributor",
+    "rhutik",
+    "moon",
+    "9735552",
+    0
+  );
+  const test = borsh.serialize(SCHEMA, data);
+  const Buffers = Buffer.from(test);
+  //@ts-ignore
+  const t = borsh.deserialize(SCHEMA, DistributorAccount, Buffers)
+  console.log(t);
+  
+  return (
+    <>
+      <div>
+        <h1>Hello TS</h1>
+      </div>
+    </>
+  );
+};
