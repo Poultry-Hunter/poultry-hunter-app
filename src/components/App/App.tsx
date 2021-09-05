d707421a116a660c38bd139b8f7e5b4092ae775a
SairajK19 <saikapdi11@gmail.com>
2021-09-05 13:39:05 +0530

    changed endpoint to devnet

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -42,7 +42,7 @@ export const App = (): JSX.Element => {
   );
   return (
     <>
-      <ConnectionProvider endpoint="http://127.0.0.1:8899">
+      <ConnectionProvider endpoint={endpoint}>
         <WalletProvider wallets={wallets} autoConnect>
           <WalletDialogProvider>
             <ToastContainer
